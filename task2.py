a514143118127e1865cdf4051faa71c6808ecce7
Sanchit <sanchitk330@gmail.com>
2025-11-28 23:53:17 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# Take user's first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate to full name
+full_name = first_name + " " + last_name
+
+# Personalized greeting
+print("Hello," ,full_name,"! Welcome to the Python Program.")
